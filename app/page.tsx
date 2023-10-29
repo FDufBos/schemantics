6728e1805872565935bc7c21ebd7024c73e5abad
Francesco <fduffyboscagli@gmail.com>
2023-10-29 13:58:50 -0700

    Refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,12 @@
 "use client";
 
-import Image from "next/image";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+
+import { DocumentTypeSelect } from "@/components/DocumentTypeSelect";
+import { InputField } from "@/components/InputField";
+import { ItemList } from "@/components/ItemList";
+import { OutputPre } from "@/components/OutputPre";
+import { FooterSection } from "@/components/ItemsDrawer";
 
 import React, { useState } from "react";
 
@@ -89,263 +85,202 @@ export default function Home() {
       bg: "bg-violet-500/10",
       text: "text-violet-600",
     },
+    list: {
+      bg: "bg-slate-500/10",
+      text: "text-slate-600",
+    },
   };
 
+  const handleItemChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    // Get the new value from the event and replace spaces with underscores
+    let newValue = event.target.value.replace(/\s/g, "_");
+
+    // Get the old value from the allItems array using the index
+    const oldValue = allItems[index];
+
+    // Check if the new value already exists in allItems or listItems
+    // If it does, append a timestamp to make it unique
+    if (allItems.includes(newValue) || listItems.has(newValue)) {
+      newValue = `${newValue}_${Date.now()}`;
+    }
+
+    // Update the output state
+    setOutput((prevOutput) => {
+      // Create a new array from the entries of the fields map in the previous output
+      // For each entry, check if the key matches the old value
+      // If it does, replace the key with the new value
+      // If it doesn't, keep the key as it is
+      const newFieldsArray: Array<[string, any]> = Array.from(
+        prevOutput.config.fields.entries()
+      ).map(([key, value]): [string, any] => {
+        if (typeof oldValue === "object" && oldValue.type === "list") {
+          return key === oldValue.value ? [newValue, value] : [key, value];
+        } else {
+          return key === oldValue ? [newValue, value] : [key, value];
+        }
+      });
+
+      const newFields = new Map(newFieldsArray);
+
+      // Return the new output object
+      return {
+        ...prevOutput,
+        config: {
+          ...prevOutput.config,
+          fields: newFields,
+        },
+      };
+    });
+
+    // Update the allItems state
+    // For each item, check if the index matches the index of the changed item
+    // If it does, replace the item with the new value
+    // If it doesn't, keep the item as it is
+    setAllItems((prevAllItems) =>
+      prevAllItems.map((item, itemIndex) =>
+        itemIndex === index ? newValue : item
+      )
+    );
+
+    // If the old value is a list item, update the listItems state
+    if (typeof oldValue === "object" && oldValue.type === "list") {
+      setListItems((prevListItems) => {
+        // Create a new map from the previous list items
+        const newListItems = new Map(prevListItems);
+
+        // Set the new value in the new list items map to the value of the old value
+        newListItems.set(newValue, newListItems.get(oldValue.value));
+
+        // Delete the old value from the new list items map
+        newListItems.delete(oldValue.value);
+
+        // Return the new list items map
+        return newListItems;
+      });
+    }
+  };
+
+  const handleItemBlur = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    let newValue = event.target.value.replace(/\s/g, "_"); // Replace spaces with underscores
+    const oldValue = allItems[index];
+
+    // Check if the new value already exists in allItems or listItems
+    if (
+      allItems.some(
+        (item, itemIndex) =>
+          itemIndex !== index &&
+          (item === newValue || (item.value && item.value === newValue))
+      ) ||
+      listItems.has(newValue)
+    ) {
+      newValue = `${newValue}_${Date.now()}`; // Append a timestamp to make it unique
+    }
+
+    setOutput((prevOutput) => {
+      const newFieldsArray = Array.from(prevOutput.config.fields.entries()).map(
+        ([key, value]) => {
+          // Check if the item is a list item
+          if (typeof oldValue === "object" && oldValue.type === "list") {
+            return key === oldValue.value ? [newValue, value] : [key, value];
+          } else {
+            return key === oldValue ? [newValue, value] : [key, value];
+          }
+        }
+      );
+      const newFields = new Map(newFieldsArray);
+
+      return {
+        ...prevOutput,
+        config: {
+          ...prevOutput.config,
+          fields: newFields,
+        },
+      };
+    });
+
+    setAllItems((prevAllItems) =>
+      prevAllItems.map((item, itemIndex) =>
+        itemIndex === index
+          ? typeof item === "object"
+            ? { type: item.type, value: newValue }
+            : newValue
+          : item
+      )
+    );
+
+    // Check if the item is a list item
+    if (typeof oldValue === "object" && oldValue.type === "list") {
+      setListItems((prevListItems) => {
+        const newListItems = new Map(prevListItems);
+        newListItems.set(newValue, newListItems.get(oldValue.value));
+        newListItems.delete(oldValue.value);
+        return newListItems;
+      });
+    }
+  };
   return (
     <main className="flex flex-col items-center w-full bg-slate-50">
       <section className="flex justify-center w-full gap-8 px-4 min-h-[calc(100vh+100vh)]">
         <div className="min-h-screen w-[60%] bg-white border border-gray-100 mt-8 rounded-t-lg">
           <div className="flex gap-4 p-4">
-            <div>
-              <Label htmlFor="docType">Document Type</Label>
-              <Select
-                onValueChange={(value) =>
-                  setOutput((prevOutput) => ({ ...prevOutput, type: value }))
-                }
-              >
-                <SelectTrigger className="w-[180px]">
-                  <SelectValue placeholder="Document type" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="page">Page</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-            <div>
-              <Label htmlFor="slug">Slug</Label>
-              <Input
-                type="text"
-                id="slug"
-                placeholder="slug"
-                onChange={(event) =>
-                  setOutput((prevOutput) => ({
-                    ...prevOutput,
-                    slug: event.target.value,
-                  }))
-                }
-              />
-            </div>
-            <div>
-              <Label htmlFor="name">Title</Label>
-              <Input
-                type="text"
-                id="name"
-                placeholder="Title"
-                onChange={(event) =>
-                  setOutput((prevOutput) => ({
-                    ...prevOutput,
-                    name: event.target.value,
-                  }))
-                }
-              />
-            </div>
-            <div>
-              <Label htmlFor="path">Path</Label>
-              <Input
-                type="text"
-                id="path"
-                placeholder="/"
-                onChange={(event) =>
-                  setOutput((prevOutput) => ({
-                    ...prevOutput,
-                    path: event.target.value,
-                  }))
-                }
-              />
-            </div>
+            <DocumentTypeSelect
+              onValueChange={(value) =>
+                setOutput((prevOutput) => ({ ...prevOutput, type: value }))
+              }
+            />
+            <InputField
+              id="slug"
+              placeholder="slug"
+              onChange={(event) =>
+                setOutput((prevOutput) => ({
+                  ...prevOutput,
+                  slug: event.target.value,
+                }))
+              }
+            />
+            <InputField
+              id="name"
+              placeholder="Title"
+              onChange={(event) =>
+                setOutput((prevOutput) => ({
+                  ...prevOutput,
+                  name: event.target.value,
+                }))
+              }
+            />
+            <InputField
+              id="path"
+              placeholder="/"
+              onChange={(event) =>
+                setOutput((prevOutput) => ({
+                  ...prevOutput,
+                  path: event.target.value,
+                }))
+              }
+            />
           </div>
           <div className="px-4 pb-3">
             <Separator />
           </div>
-          <div className="flex flex-col gap-2 px-4 py-2">
-            {allItems.map((item, index) => {
-              const value = typeof item === "string" ? item : item.value;
-              const fieldType = output.config.fields.get(value)?.type;
-              const colors =
-                fieldTypeColors[fieldType as keyof typeof fieldTypeColors];
-              return (
-                <div key={index} className="relative cursor-auto select-select">
-                  <span
-                    className={`absolute px-[10px] py-[2px] text-xs -translate-y-1/2 rounded-full right-2 top-1/2 ${colors?.bg} ${colors?.text}`}
-                  >
-                    {fieldType}
-                  </span>
-                  <div className="flex items-center gap-1 cursor-grab active:cursor-grabbing">
-                    <svg
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="row-grabber"
-                    >
-                      <path
-                        fill="#E4E6EA"
-                        d="M11 18c0 1.1-.9 2-2 2s-2-.9-2-2s.9-2 2-2s2 .9 2 2zm-2-8c-1.1 0-2 .9-2 2s.9 2 2 2s2-.9 2-2s-.9-2-2-2zm0-6c-1.1 0-2 .9-2 2s.9 2 2 2s2-.9 2-2s-.9-2-2-2zm6 4c1.1 0 2-.9 2-2s-.9-2-2-2s-2 .9-2 2s.9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2s2-.9 2-2s-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2s2-.9 2-2s-.9-2-2-2z"
-                      />
-                    </svg>
-                    <Input
-                      type="text"
-                      value={value}
-                      onChange={(event) => {
-                        const newValue = event.target.value.replace(/\s/g, "_"); // Replace spaces with underscores
-
-                        setOutput((prevOutput) => {
-                          const newFields = new Map();
-                          for (const [key, field] of Array.from(
-                            prevOutput.config.fields.entries()
-                          )) {
-                            if (key === value) {
-                              newFields.set(newValue, field);
-                            } else {
-                              newFields.set(key, field);
-                            }
-                          }
-                          return {
-                            ...prevOutput,
-                            config: {
-                              ...prevOutput.config,
-                              fields: newFields,
-                            },
-                          };
-                        });
-
-                        if (typeof item === "string") {
-                          setItems((prevItems) =>
-                            prevItems.map((prevItem) =>
-                              prevItem === value ? newValue : prevItem
-                            )
-                          );
-                          setAllItems((prevAllItems) =>
-                            prevAllItems.map((prevItem) =>
-                              prevItem === value ? newValue : prevItem
-                            )
-                          );
-                        } else {
-                          setListItems(
-                            (prevListItems) =>
-                              new Map(
-                                Array.from(prevListItems.entries()).map(
-                                  ([prevKey, prevValue]) =>
-                                    prevKey === value
-                                      ? [newValue, prevValue]
-                                      : [prevKey, prevValue]
-                                )
-                              )
-                          );
-                        }
-                      }}
-                      onBlur={(event) => {
-                        let newValue = event.target.value;
-                        if (newValue === "") {
-                          // If input is empty on unfocus
-                          const uniqueValue = `${fieldType}_${Date.now()}`; // Create a unique value based on fieldType and current timestamp
-                          newValue = uniqueValue; // Set the input value to the unique value
-                        }
-                        newValue = newValue.replace(/\s/g, "_"); // Replace spaces with underscores
-                        event.target.value = newValue; // Update the input value
-
-                        setOutput((prevOutput) => {
-                          const newFields = new Map(prevOutput.config.fields);
-                          if (!newFields.has(newValue)) {
-                            newFields.set(newValue, newFields.get(value)); // Update the value in the fields map
-                            newFields.delete(value); // Remove the old value from the fields map
-                          }
-
-                          return {
-                            ...prevOutput,
-                            config: {
-                              ...prevOutput.config,
-                              fields: newFields,
-                            },
-                          };
-                        });
-
-                        if (typeof item === "string") {
-                          setItems((prevItems) =>
-                            prevItems.map((prevItem) =>
-                              prevItem === value ? newValue : prevItem
-                            )
-                          );
-                          setAllItems((prevAllItems) =>
-                            prevAllItems.map((prevItem) =>
-                              prevItem === value ? newValue : prevItem
-                            )
-                          );
-                        } else {
-                          setListItems(
-                            (prevListItems) =>
-                              new Map(
-                                Array.from(prevListItems.entries()).map(
-                                  ([prevKey, prevValue]) =>
-                                    prevKey === value
-                                      ? [newValue, prevValue]
-                                      : [prevKey, prevValue]
-                                )
-                              )
-                          );
-                        }
-                      }}
-                    />
-                  </div>
-                </div>
-              );
-            })}
-          </div>
+          <ItemList
+            items={allItems}
+            onItemChange={handleItemChange}
+            onItemBlur={handleItemBlur}
+            output={output}
+            fieldTypeColors={fieldTypeColors}
+          />
         </div>
         <div className="min-h-screen w-[40%] bg-slate-100 border- border-gray-50 mt-8 rounded-t-lg text-xs text-slate-700 p-4 pb-32">
-          <pre>
-            {JSON.stringify(
-              {
-                ...output,
-                config: {
-                  ...output.config,
-                  fields: Object.fromEntries(output.config.fields),
-                },
-              },
-              null,
-              2
-            )}
-          </pre>{" "}
+          <OutputPre output={output} />
         </div>
       </section>
-      <section className="fixed bottom-0 left-0 flex items-center justify-center w-full h-24 gap-8 text-gray-500 border-t rounded-t-xl border-slate-100 bg-gray-400/20 backdrop-blur-lg">
-        <div
-          className="cursor-pointer select-none"
-          onClick={() => handleItemClick("string")}
-        >
-          STRING
-        </div>
-        <div
-          className="cursor-pointer select-none"
-          onClick={() => handleItemClick("text")}
-        >
-          TEXT
-        </div>
-        <div
-          className="cursor-pointer select-none"
-          onClick={() => handleItemClick("wysiwyg")}
-        >
-          WYSIWYG
-        </div>
-        <div
-          className="cursor-pointer select-none"
-          onClick={() => handleItemClick("url")}
-        >
-          URL
-        </div>
-        <div
-          className="cursor-pointer select-none"
-          onClick={() => handleItemClick("image")}
-        >
-          IMAGE
-        </div>
-        <div
-          className="cursor-pointer select-none"
-          onClick={() => handleItemClick("list")}
-        >
-          LIST
-        </div>
-      </section>
+      <FooterSection onItemAdd={handleItemClick} />
     </main>
   );
 }
